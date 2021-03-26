0c6ae4774ca17a29a10828a4f9c143337da8c909
Samuel Ifere <sml.ifere@gmail.com>
2021-03-26 15:08:54 +0000

    Initial Project setup

---
+++ src/main/java/com/sammy/SpringPlayGroundApplication.java
@@ -0,0 +1,12 @@
+package com.sammy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class SpringPlayGroundApplication {
+    public static void main(String[] args) {
+        run(SpringPlayGroundApplication.class, args);
+    }
+}
