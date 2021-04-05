83ea36e785b7a05a2eb0f2f84a6a61648802d5f6
Samuel Ifere <sml.ifere@gmail.com>
2021-04-05 12:26:56 +0100

    Adds Region enum JPA converter to store enum values in DB

--- src/main/java/com/sammy/entity/enums/Region.java
+++ src/main/java/com/sammy/entity/enums/Region.java
@@ -1,11 +1,15 @@
 package com.sammy.entity.enums;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 
+@Getter
 public enum Region {
     Central_Coast("Central Coast"),
     Southern_California("Southern California"),
     Northern_California("Northern California"),
+    Napa_Sonoma_Counties("Napa/Sonoma Counties"),
     Varies("Varies");
 
     private final String label;
@@ -15,10 +19,18 @@ public enum Region {
     }
 
     public static Region findByLabel(String byLabel){
-        return Arrays.stream(Region.values())
+        return switch (byLabel) {
+            case "Central Coast" -> Region.Central_Coast;
+            case "Southern California" -> Region.Southern_California;
+            case "Northern California" -> Region.Northern_California;
+            case "Napa/Sonoma Counties" -> Region.Napa_Sonoma_Counties;
+            case "Varies" -> Region.Varies;
+            default -> throw new IllegalArgumentException("Label [" + byLabel + "] not supported.");
+        };
+        /*return Arrays.stream(Region.values())
                      .filter(region -> region.label.equalsIgnoreCase(byLabel))
                      .findFirst()
-                     .orElse(null);
+                     .orElse(null);*/
 
     }
 }
