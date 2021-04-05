4fc4144670f6109673b3899a6b5d724ccf6c6eb5
Samuel Ifere <sml.ifere@gmail.com>
2021-04-05 22:07:53 +0100

    Adds logic to restrict DML data endpoints and modifies TourPackage endpoint

--- src/main/java/com/sammy/respository/TourRepository.java
+++ src/main/java/com/sammy/respository/TourRepository.java
@@ -1,9 +1,57 @@
 package com.sammy.respository;
 
 import com.sammy.entity.business.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TourRepository extends JpaRepository<Tour, Integer> {
+    //List<Tour> findByTourPackageCode(@Param("code") String tourPackageCode);
+    Page<Tour> findByTourPackageCode(@Param("code") String tourPackageCode, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> List<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void flush();
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S saveAndFlush(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteInBatch(Iterable<Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllInBatch();
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
