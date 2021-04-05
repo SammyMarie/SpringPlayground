6e87d7907b4f3a3fb86c045c0d805185fa992054
Samuel Ifere <sml.ifere@gmail.com>
2021-04-05 12:28:47 +0100

    Adds custom query method to search by name.

--- src/main/java/com/sammy/respository/TourPackageRepository.java
+++ src/main/java/com/sammy/respository/TourPackageRepository.java
@@ -4,6 +4,9 @@ import com.sammy.entity.business.TourPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
+    Optional<TourPackage> findByName(String name);
 }
