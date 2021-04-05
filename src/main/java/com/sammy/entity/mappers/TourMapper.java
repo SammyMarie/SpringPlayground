a724f3d9a8a4c9cc45c6fdc91bb9586bac735193
Samuel Ifere <sml.ifere@gmail.com>
2021-04-05 12:31:29 +0100

    Updates Tour mapper to convert new api object to business entity.

--- src/main/java/com/sammy/entity/mappers/TourMapper.java
+++ src/main/java/com/sammy/entity/mappers/TourMapper.java
@@ -1,26 +1,39 @@
 package com.sammy.entity.mappers;
 
 import com.sammy.entity.business.Tour;
+import com.sammy.entity.business.TourPackage;
+import com.sammy.entity.enums.Difficulty;
+import com.sammy.entity.enums.Region;
 import com.sammy.entity.resource.TourApi;
+import com.sammy.entity.resource.TourPackageApi;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public final class TourMapper {
 
-    public static Tour toBusiness(TourApi tourApi) {
+    public static Tour toBusiness(TourApi tourApi, TourPackage tourPackage) {
+
+        TourPackage packagedTour = Optional.ofNullable(tourPackage)
+                                           .orElseGet(() -> TourPackageMapper.toBusiness(TourPackageApi.builder()
+                                                                                                       .name(tourApi.getTourPackageName())
+                                                                                                       .build()));
+
         return Tour.builder()
                    .tourId(tourApi.getTourId())
                    .title(tourApi.getTitle())
                    .description(tourApi.getDescription())
                    .blurb(tourApi.getBlurb())
-                   .price(tourApi.getPrice())
+                   .price(new BigDecimal(tourApi.getPrice()))
                    .duration(tourApi.getDuration())
                    .bullets(tourApi.getBullets())
                    .keywords(tourApi.getKeywords())
-                   .tourPackage(tourApi.getTourPackage())
-                   .difficulty(tourApi.getDifficulty())
-                   .region(tourApi.getRegion())
+                   .tourPackage(packagedTour)
+                   .difficulty(Difficulty.valueOf(tourApi.getDifficulty()))
+                   .region(Region.findByLabel(tourApi.getRegion()))
                    .build();
     }
 
@@ -30,13 +43,13 @@ public final class TourMapper {
                       .title(tour.getTitle())
                       .description(tour.getDescription())
                       .blurb(tour.getBlurb())
-                      .price(tour.getPrice())
+                      .price(tour.getPrice().toPlainString())
                       .duration(tour.getDuration())
                       .bullets(tour.getBullets())
                       .keywords(tour.getKeywords())
-                      .tourPackage(tour.getTourPackage())
-                      .difficulty(tour.getDifficulty())
-                      .region(tour.getRegion())
+                      .tourPackageName(tour.getTourPackage().getName())
+                      .difficulty(tour.getDifficulty().name())
+                      .region(tour.getRegion().name())
                       .build();
     }
 }
