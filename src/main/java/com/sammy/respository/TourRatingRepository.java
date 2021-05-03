41b93b8c45b020cca6438386bae13c55307938c5
Samuel Ifere <sml.ifere@gmail.com>
2021-05-03 17:32:05 +0100

    Adds pageable custom method

--- src/main/java/com/sammy/respository/TourRatingRepository.java
+++ src/main/java/com/sammy/respository/TourRatingRepository.java
@@ -2,6 +2,8 @@ package com.sammy.respository;
 
 import com.sammy.entity.business.TourRating;
 import com.sammy.entity.business.TourRatingPk;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -12,6 +14,6 @@ import java.util.Optional;
 public interface TourRatingRepository extends JpaRepository<TourRating, TourRatingPk> {
 
     List<TourRating> findByRatingPkTourTourId(int tourId);
-
+    Page<TourRating> findByRatingPkTourTourId(int tourId, Pageable pageable);
     Optional<TourRating> findByRatingPkTourTourIdAndRatingPkCustomerId(int tourId, int customerId);
 }
